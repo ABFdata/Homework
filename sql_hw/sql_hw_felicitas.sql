5b923c7133ceb97ace2329110d7d7d23b19e032b
ABFdata <andyfelicitas@gmail.com>
2018-01-10 10:44:02 -0800

    first_upload_sql_hw

---
+++ sql_hw/sql_hw_felicitas.sql
@@ -0,0 +1,90 @@
+-- use sakila 
+use sakila;
+
+-- 1a. Display the first and last names of all actors from the table `actor`. 
+select first_name, last_name
+from actor;
+
+-- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column `Actor Name`. 
+select concat(first_name, ' ', last_name) 
+AS 'actor_name' 
+from actor;
+
+select * from actor;
+
+-- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, 
+-- "Joe." What is one query would you use to obtain this information?
+select actor_id, first_name, last_name
+from actor
+where first_name = 'Joe';
+
+-- 2b. Find all actors whose last name contain the letters `GEN`:
+select first_name, last_name
+from actor 
+where last_name like '%GEN%';
+
+-- 2c. Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first 
+-- name, in that order:
+select last_name, first_name
+from actor
+where last_name like '%LI%';
+
+-- 2d. Using `IN`, display the `country_id` and `country` columns of the 
+-- following countries: Afghanistan, Bangladesh, and China:
+select country_id, country
+from country 
+where country = 'Afghanistan' or country = 'Bangladesh' or country = 'China';
+
+-- * 3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. 
+-- Hint: you will need to specify the data type.
+alter table actor
+add middle_name varchar(50) after first_name;
+
+select * from actor;
+
+-- You realize that some of these actors have tremendously long last names. 
+-- Change the data type of the `middle_name` column to `blobs`.
+alter table actor
+change middle_name middle_name blob;
+select * from actor;
+
+-- 3c. Now delete the `middle_name` column.
+alter table actor
+drop column middle_name;
+
+select * from actor;
+
+-- 4a. List the last names of actors, as well as how many actors have that last name.
+select last_name, count(*) as count
+from actor
+group by last_name
+having count(*)>0;
+
+-- 4b. List last names of actors and the number of actors who have that last name, but only for names 
+-- that are shared by at least two actors
+select last_name, count(*) as count
+from actor
+group by last_name
+having count(*)>1;
+
+-- Oh, no! The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`, 
+-- the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+select first_name, last_name
+from actor
+where first_name = 'GROUCHO'; -- and last_name = 'WILLIAMS';
+
+update actor
+set first_name = 'HARPO'
+where first_name = 'GROUCHO' and last_name = 'WILLIAMS';
+
+select first_name, last_name
+from actor
+where first_name = 'HARPO';
+
+-- 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name 
+-- after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`. 
+-- Otherwise, change the first name to `MUCHO GROUCHO`, as that is exactly what the actor will be with the grievous error. 
+-- BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO `MUCHO GROUCHO`, 
+-- HOWEVER! (Hint: update the record using a unique identifier.)
+
+
