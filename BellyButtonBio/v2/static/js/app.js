85ee32e1a1ee9db69786d381e67069d2d7d53cf5
ABFdata <andyfelicitas@gmail.com>
2018-02-22 16:30:15 -0800

    v2

---
+++ BellyButtonBio/v2/static/js/app.js
@@ -0,0 +1,47 @@
+// Step 2 - Part 1
+// populate drop down menu /names
+
+// document.getElementById - id="selDataset"
+
+
+// document.createElement
+
+// append
+
+
+// function optionChanged () {
+//     document.getElementById('test').innerHTML = "Select Sample ID"
+// };
+
+// optionChanged();
+
+
+
+
+
+
+
+
+
+
+// var url = "/samples/<sample>";
+
+// function buildPieChart() {
+//     Plotly.d3.json(url, function (error, response) {
+
+//         console.log(response);
+//         var trace1 = {
+//             type: "pie",
+//             //mode: ""
+//             name: "Top 10 Samples",
+//             labels: response.map(url => data.otu_ids),
+//             values: response.map(url => data.sample_values),
+
+
+//         };
+
+//         Plotly.newPlot("plot", data, layout);
+//     });
+// }
+
+// buildPieChart();
