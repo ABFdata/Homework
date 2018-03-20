d9138f1cf07d36444bc8a1f08c5f084458519617
ABFdata <andyfelicitas@gmail.com>
2018-03-20 12:18:44 -0700

    add time & timeline js for slider

--- Leaflet/ABFMap/js/time.js
+++ Leaflet/ABFMap/js/time.js
@@ -1,17 +1,10 @@
 // Create map object 
 var map = L.map("map", {
-    center: [37.09, -95.71],
+    center: [40.7, -94.5],
     zoom: 4,
-    timeDimension: true,
-    timeDimensionOptions: {
-        timeInterval: "2018-02-28/2018-03-07",
-        period: "PT1H"
-    },
-    timeDimensionControl: true,
+    layers: baseMaps
 
 });
-
-
 
 // add api key to variable 
 var apiKey = "access_token=pk.eyJ1IjoiYWJmZGF0YSIsImEiOiJjamU2aHlrZTgwMGdxMzNxa3R3OG5wZmNkIn0._No3joCSQ0ZhN2KE30LC8w";
@@ -20,16 +13,16 @@ var apiKey = "access_token=pk.eyJ1IjoiYWJmZGF0YSIsImEiOiJjamU2aHlrZTgwMGdxMzNxa3
 //L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" + apiKey).addTo(map);
 
 // Outdoors - default map
-var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" + apiKey, 
-{id: 'map'});
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" + apiKey,
+    { id: 'map' });
 
 // Satellite map
 var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" + apiKey,
-{id: 'map'});
+    { id: 'map' });
 
 // Grayscale map
 var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" + apiKey,
-{id: 'map'});
+    { id: 'map' });
 
 // Grayscale.addTo(map);
 
@@ -41,33 +34,35 @@ var baseMaps = {
 
 grayscale.addTo(map);
 
+// We create the layers for our two different sets of data, earthquakes and tectonicplates.
+var tectonicplates = new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
+var timelineLayer = new L.LayerGroup();
 
+// We define an object that contains all of our overlays.
+// Any combination of these overlays may be visible at the same time!
+var overlays = {
+    "Earthquake Timeline": timelineLayer,
+    "Tectonic Plates": tectonicplates,
+    "All Earthquakes": earthquakes
+};
 
 // earthquakes all week data
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
 var plates_link = "../json/plates.json"
+var controlLayers = L.control.layers(baseMaps, overlays).addTo(map);
 
-var controlLayers = L.control.layers(baseMaps).addTo(map);
-
-
-// TIME DIMENSION TEST
-
-// var timeSeries = L.timeDimension(earthquakeLayer).addTo(map);
-
-// L.timeDimension.layer.geoJson(layer).addTo(map);
-
-
- function styleInfo(feature){
-     return{
-     fillOpacity: 0.80,
-     color: getColor(feature.properties.mag),
-     radius: getRadius(feature.properties.mag),
-     stroke: true,
-     //border: true,
-     weight: 0.5
+// Create a sytle function for the circles
+function styleInfo(feature) {
+    return {
+        fillOpacity: 0.80,
+        color: getColor(feature.properties.mag),
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        //border: true,
+        weight: 0.5
     };
- };
+};
 
 // COLORS 2:
 // #DC143C crimson 	   	
@@ -77,55 +72,52 @@ var controlLayers = L.control.layers(baseMaps).addTo(map);
 // #1E90FF dodgerblue
 // #B0E0E6 powderblue  
 
- function getColor(magnitude){
-    switch(true) {
+function getColor(magnitude) {
+    switch (true) {
         case magnitude > 5:
-        return "#DC143C"; // crimson
+            return "#DC143C"; // crimson
         case magnitude > 4:
-        return "#FFFF00"; // yellow
+            return "#FFFF00"; // yellow
         case magnitude > 3:
-        return "#00CED1"; // darkturquoise
+            return "#00CED1"; // darkturquoise
         case magnitude > 2:
-        return "#9400D3"; // darkviolet
+            return "#9400D3"; // darkviolet
         case magnitude > 1:
-        return "#1E90FF"; // dodgerblue
+            return "#1E90FF"; // dodgerblue
         default:
-        return "#B0E0E6"; // powderblue
+            return "#B0E0E6"; // powderblue
     }
 };
-
 
 // function get Radius
-
- function getRadius(magnitude){
-    switch(true) {
+function getRadius(magnitude) {
+    switch (true) {
         case magnitude > 5:
-        return 20;
+            return 20;
         case magnitude > 4:
-        return 13;
+            return 13;
         case magnitude > 3:
-        return 10;
+            return 10;
         case magnitude > 2:
-        return 7;
+            return 7;
         case magnitude > 1:
-        return 6;
+            return 6;
         default:
-        return 5;
+            return 5;
     }
 };
-
 
 //create a function: populateInfo to add data
 function populateInfo(feature, layer) {
     layer.bindPopup("<h1 class='infoHeader'>Weekly Earthquake Data</h1> \
     <p class='description'>" + "Location: " + feature.properties.place + "</p>\
     <p class='description'>" + "Magnitude: " + feature.properties.mag + "</p>");
-        
+
 };
 
 // function for plate color
-function colorPlates(feature){
-    return{
+function colorPlates(feature) {
+    return {
         color: "#FFA500",
         fillOpacity: 0.05
     };
@@ -135,82 +127,98 @@ function colorPlates(feature){
 function popPlate(feature, layer) {
     layer.bindPopup("<h1 class='infoHeader'>Tectonic Plate:</h1> \
 <p class='plate'>" + feature.properties.PlateName + "</p>");
-        
+
 };
 
+// Timeline Layer
+d3.json(link, function (data) {
+    // This function returns an interval of time during which the earthquake will be visible on the map.
+    var getInterval = function (quake) {
+        // We need a start and end time.
+        // Since we only have a start time, we create an end time based on the magnitude of the earthquake.
+        return {
+            start: quake.properties.time,
+            end: quake.properties.time + quake.properties.mag * 1800000 * 2
+        };
+    };
+
+    // Here we create a timeline control object on our map.
+    var timelineControl = L.timelineSliderControl({
+        formatOutput: function (date) {
+            return new Date(date).toString();
+        },
+        // Setting the number of 'ticks' or frames that will be displayed on our map.
+        steps: 2000
+    });
+
+    // We also create the timeline layer.
+    var timeline = L.timeline(data, {
+        getInterval: getInterval,
+        pointToLayer: function (layerData, latlng) {
+            return L.circleMarker(latlng, {
+                radius: layerData.properties.mag * 6,
+                color: getColor(layerData.properties.mag),
+                opacity: 0.75,
+                fillOpacity: 0.75,
+                weight: 0
+            }).bindPopup(
+                "Magnitude: " +
+                layerData.properties.mag +
+                "<br>Location: " +
+                layerData.properties.place
+            );
+        }
+    });
+    timelineControl.addTo(map);
+    timelineControl.addTimelines(timeline);
+    timeline.addTo(timelineLayer);
+    timelineLayer.addTo(map);
+
+})
+
 // Here we add a GeoJSON layer to the map once the file is loaded.
-d3.json(link, function(data){
+d3.json(link, function (data) {
     var earthquakeLayer = L.geoJson(data, {
         // We turn each feature into a circleMarker on the map.
-        pointToLayer: function(feature, latlng) {
-          return L.circleMarker(latlng);
-          
-          
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
         },
         // We set the style for each circleMarker using our styleInfo function.
         style: styleInfo,
         // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
         onEachFeature: populateInfo,
-        //timeSeries
-        //timeSeries,
-        
-        
-      }).addTo(map);
-      controlLayers.addOverlay(earthquakeLayer, 'Earthquakes');
-      
-    });
+    }).addTo(earthquakes);
+});
 
 // add plate layer
-d3.json(plates_link, function(data) {
+d3.json(plates_link, function (data) {
     // Creating a GeoJSON layer with the retrieved data
-    var plateLayer  = L.geoJson(data, {
+    var plateLayer = L.geoJson(data, {
         style: colorPlates,
         // onEachFeature - popPlate to add popup for each plate
         onEachFeature: popPlate,
-    }).addTo(map);
-    controlLayers.addOverlay(plateLayer, "Fault Lines");
-        
-   
+    }).addTo(tectonicplates);
+    // controlLayers.addOverlay(plateLayer, "Fault Lines");
 
-// Setting up the legend
-var legend = L.control({position: 'bottomleft'});
-legend.onAdd = function (map) {
+    // Setting up the legend
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function (map) {
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 1, 2, 3, 4, 5],
-        labels = [];
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 1, 2, 3, 4, 5],
+            labels = [];
 
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
 
-    return div;
-};
+        return div;
+    };
 
-legend.addTo(map);
+    legend.addTo(map);
 
 });
 
-// script for time slider
-
-// var getInterval = function(quake) {
-//     return {
-//         start: quake.properties.time,
-//         end: quake.properties.time + quake.properties.mag * 1000000 * 2
-// };
-// };
-
-
-
-// var timelineControl = L.timelineSliderControl({
-//     formatOutput: function(date) {
-//         return new Date(date).toString();
-//     },
-//     steps: 200
-// });
-
-// slider control
-// timeline
